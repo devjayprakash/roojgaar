ef068625bde83364d48d3bba4132a1cd434246f0
Mind0Bender <mrcircuit1234@gmail.com>
2021-03-20 08:15:20 +0530

    Added 404 page

---
+++ frontend/src/pages/notfound/index.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import NotFoundImg from "./notfound.png";
+
+const NotFoundPage = ({ className }) => {
+  if (!className) className = "";
+  return (
+    <div
+      className={
+        "w-full flex h-full justify-center items-center py-52 text-center px-16 " +
+        className
+      }
+    >
+      <div className="max-w-lg">
+        <img draggable={false} src={NotFoundImg} alt="" srcset="" />
+        <h1 className="text-6xl font-bold">Oops, an ERROR occured!</h1>
+        <br />
+        <h3 className="text-5xl">This is not the page You were looking for.</h3>
+      </div>
+      <br />
+      <hr
+        style={{
+          borderWidth: "2px",
+          height: "50vh",
+        }}
+        className="rounded-full mx-7"
+      />
+      <br />
+      <div>
+        <h4 className="text-xl">Here are some helpful pages instead:</h4>
+        <ul className="">
+          <li>
+            <Link className="text-pink-600 text-lg" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="text-pink-600 text-lg" to="/signup">
+              Sign Up
+            </Link>
+          </li>
+          <li>
+            <Link className="text-pink-600 text-lg" to="/login">
+              Login
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;

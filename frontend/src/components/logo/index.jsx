bb2ad8f50c2a42bf57ea68e30a81a503c4be32e9
Mind0Bender <mrcircuit1234@gmail.com>
2021-03-20 08:09:47 +0530

    Added LOGO component

---
+++ frontend/src/components/logo/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Logo = ({ className }) => {
+  if (!className) className = "";
+  return (
+    <Link to="/" className={"flex " + className}>
+      <div
+        style={{
+          color: "cyan",
+        }}
+        className="text-5xl font-bold"
+      >
+        रोज़
+      </div>
+      <div className="text-5xl font-bold text-white">Gaar</div>
+    </Link>
+  );
+};
+
+export default Logo;

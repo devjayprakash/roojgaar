83c5a7c4698df71ca5ddd7e5ceab4155816e61bb
krishna9304 <krishhtrishh9304@gmail.com>
2021-03-20 23:38:46 +0530

    worker homepage

---
+++ frontend/src/components/selectedHireComp/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./index.css"
+
+const SelectedHireComp = () =>{
+    return (
+        <div className="comp">
+            <div className="left">
+                <div className="proPic"></div>
+                <div className="name">Company 1</div>
+            </div>
+            <div className="right">
+                <div className="stats"></div>
+                <div className="expand"></div>
+            </div>
+        </div>
+    )
+}
+
+export default SelectedHireComp;

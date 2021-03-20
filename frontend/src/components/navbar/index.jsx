45f477088a3c40e9c2f8be3ce4addd0e1341eea6
Mind0Bender <mrcircuit1234@gmail.com>
2021-03-20 08:12:03 +0530

    Added Navbar

---
+++ frontend/src/components/navbar/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Logo from "../logo";
+import searchlogo from "./search.png";
+
+const NavBar = ({ withSearch, className }) => {
+  if (!className) className = "";
+  return (
+    <nav
+      className={
+        "flex bg-gradient-to-br from-purple-700 to-blue-400 py-3 justify-between " +
+        className
+      }
+    >
+      <Logo className="ml-10" />
+      {withSearch && (
+        <div className="rounded-full flex w-1/2 outline-none">
+          <input
+            className="px-10 w-full h-full rounded-full outline-none"
+            type="search"
+            placeholder={"Search for Services"}
+          />
+          <div className="ml-3 rounded-full cursor-pointer hover:shadow-xl hover:bg-gray-100 p-3 duration-300">
+            <img
+              src={searchlogo}
+              style={{
+                maxHeight: "2rem",
+              }}
+              alt=""
+            />
+          </div>
+        </div>
+      )}
+      <div className="rounded-xl h-10 px-5 py-2 text-center mt-1 font-bold text-white mr-10 hover:shadow-xl bg-pink-500 duration-200">
+        Logout
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
